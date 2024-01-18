869627581178c04cbf24d7156300ab86a9be2dd5
mylesh.dev <mylesh.dev@gmail.com>
2024-01-18 12:42:23 +0100

    Add movement and center camera

--- level/level.gd
+++ level/level.gd
@@ -27,6 +27,9 @@ const LAYER_MAP = {
 	LAYER_KEY_BOXES: BOX_LAYER
 }
 
+var _moving: bool = false
+var _total_moves: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setup_level()
@@ -34,7 +37,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	if _moving == true:
+		return
+	
+	var move_direction = Vector2i.ZERO
+	
+	if Input.is_action_just_pressed("right") == true:
+		player.flip_h = false
+		move_direction = Vector2i.RIGHT
+	if Input.is_action_just_pressed("left") == true:
+		player.flip_h = true
+		move_direction = Vector2i.LEFT
+	if Input.is_action_just_pressed("up") == true:
+		move_direction = Vector2i.UP
+	if Input.is_action_just_pressed("down") == true:
+		move_direction = Vector2i.DOWN
+	if Input.is_action_just_pressed("reload") == true:
+		pass
+	if Input.is_action_just_pressed("exit") == true:
+		pass
+	
+	if move_direction != Vector2i.ZERO:
+		player_move(move_direction)
 
 func place_player_on_tile(tile_coord: Vector2i):
 	var new_pos: Vector2 = Vector2(
@@ -43,6 +68,69 @@ func place_player_on_tile(tile_coord: Vector2i):
 	) + tile_map.global_position
 	
 	player.global_position = new_pos
+
+# GAME LOGIC
+
+func move_box(box_tile: Vector2i, direction: Vector2i) -> void:
+	var dest = box_tile + direction
+	
+	tile_map.erase_cell(BOX_LAYER, box_tile)
+	
+	if dest in tile_map.get_used_cells(TARGET_LAYER):
+		tile_map.set_cell(BOX_LAYER, dest, SOURCE_ID,
+			get_atlas_coord_for_layer_name(LAYER_KEY_TARGET_BOXES))
+	else:
+		tile_map.set_cell(BOX_LAYER, dest, SOURCE_ID,
+			get_atlas_coord_for_layer_name(LAYER_KEY_BOXES))
+
+func get_player_tile() -> Vector2i:
+	var player_offset = player.global_position - tile_map.global_position
+	return Vector2i(player_offset / GameData.TILE_SIZE)
+
+func cell_is_wall(cell: Vector2i) -> bool:
+	return cell in tile_map.get_used_cells(WALL_LAYER)
+
+func cell_is_box(cell: Vector2i) -> bool:
+	return cell in tile_map.get_used_cells(BOX_LAYER)
+
+func cell_is_empty(cell: Vector2i) -> bool:
+	return cell_is_wall(cell) == false and cell_is_box(cell) == false
+
+func box_can_move(box_tile: Vector2i, direction: Vector2i) -> bool:
+	var new_tile = box_tile + direction
+	return cell_is_empty(new_tile)
+
+func player_move(direction: Vector2i):
+	var player_tile = get_player_tile()
+	var new_tile = player_tile + direction
+	var can_move = true
+	var box_seen = false
+	
+	_moving = true
+	
+	print("direction:", direction)
+	print("player_tile:", player_tile)
+	print("new_tile:", new_tile)
+	
+	if cell_is_wall(new_tile) == true:
+		can_move = false
+		print("wall_seen")
+	if cell_is_box(new_tile) == true:
+		box_seen = true
+		can_move = box_can_move(new_tile, direction)
+		print("box_seen")
+	
+	if can_move == true:
+		print("can move")
+		_total_moves += 1
+		if box_seen == true:
+			move_box(new_tile, direction)
+		place_player_on_tile(new_tile)
+	
+	
+	_moving = false
+
+# LEVEL SETUP
 
 func get_atlas_coord_for_layer_name(layer_name: String) -> Vector2i:
 	match layer_name:
@@ -71,19 +159,30 @@ func add_layer_tiles(layer_tiles, layer_name: String) -> void:
 
 func setup_level() -> void:
 	tile_map.clear()
-	var level_data = GameData.get_data_for_level("1")
+	var level_data = GameData.get_data_for_level("2")
 	var level_tiles = level_data.tiles
 	var player_start = level_data.player_start
+	print("player_start:", player_start)
 	
 	for layer_name in LAYER_MAP.keys():
 		add_layer_tiles(level_tiles[layer_name], layer_name)
 	
 	place_player_on_tile(Vector2i(player_start.x, player_start.y))
+	move_camera()
 
 
-
-
-
+func move_camera() -> void:
+	var tnr = tile_map.get_used_rect()
+	
+	var tile_map_start_x = tnr.position.x * GameData.TILE_SIZE
+	var tile_map_end_x = tnr.size.x * GameData.TILE_SIZE + tile_map_start_x
+	
+	var tile_map_start_y = tnr.position.y * GameData.TILE_SIZE
+	var tile_map_end_y = tnr.size.y * GameData.TILE_SIZE + tile_map_start_y
+	
+	var mid_x = tile_map_start_x + (tile_map_end_x - tile_map_start_x)/2
+	var mid_y = tile_map_start_y + (tile_map_end_y - tile_map_start_y)/2 
+	camera_2d.position = Vector2(mid_x, mid_y)
 
 
 
